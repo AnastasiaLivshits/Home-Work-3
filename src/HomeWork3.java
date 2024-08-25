faf2cd39fcfe5d19b93189a043129a65204c06c7
AnastasiaLivshits <nastasia.arch@gmail.com>
2024-08-25 22:39:19 +0300

    Final

---
+++ src/HomeWork3.java
@@ -0,0 +1,106 @@
+public class HomeWork3 {
+    public static void main(String[] args) {
+        int a = 4;
+        byte b = 22;
+        short s = 38;
+        float d = 3.74f;
+        long l = 641_123_345_564_789_123L;
+        double e = 1.71111111;
+        System.out.println( a );
+        System.out.println( a );
+        System.out.println( b);
+        System.out.println( s );
+        System.out.println( d );
+        System.out.println( l );
+        System.out.println( e );
+        System.out.println(  );
+
+        double d2 = 27.12;
+        long l1 = 987_678_965_549L;
+        float f = 2.786f;
+        int sh= 569;
+        short ss= -159;
+        short i =27897;
+        byte b1= 67;
+        System.out.println( d2);
+        System.out.println( l1);
+        System.out.println( f );
+        System.out.println( sh );
+        System.out.println( ss);
+        System.out.println( i );
+        System.out.println( b1 );
+        System.out.println(  );
+        int L = 23;
+        int A=27;
+        int E=30;
+        int sum= L+A+E;
+        System.out.println("На каждого ученика рассчитано "+ (480/sum)+ " листов бумаги.");
+        System.out.println();
+        int P=8;
+        int Twenty= 8*20;
+        int Day= 8*60*24;
+        int Day3= Day*3;
+        int Month= Day*30;
+        System.out.println ("За 20 минут машина произвела "+Twenty+ " штук бутылок.");
+        System.out.println ("За день машина произвела "+Day+ " штук бутылок.");
+        System.out.println ("За 20 минут машина произвела "+Day3+ " штук бутылок.");
+        System.out.println ("За 20 минут машина произвела "+Month+ " штук бутылок.");
+        System.out.println();
+
+        int whole= 120;
+        int white=2;
+        int brown=4;
+        int C = brown+white;
+        int ClassAmount= whole/C;
+        int CansPerClassWhite= ClassAmount*white;
+        int CansPerClassBrown= ClassAmount*brown;
+        System.out.println("В школе, где"+ ClassAmount+ " классов, нужно"+ CansPerClassWhite+" банок белой краски и "+
+                CansPerClassBrown+ " банок коричневой краски.");
+
+        int Banana= 80*5;
+        int Milk= (200/100)*105;
+        int IceCream = 200;
+        int Eggs= 70*4;
+        int Weight= Banana+Milk+IceCream+Eggs;
+        float Kg= Weight/1000f;
+        System.out.println ("вес спортзавтрака в граммах равен "+ Weight);
+        System.out.println("вес спортзавтрака в килограммах равен " + Kg);
+        System.out.println();
+
+        double Days500PerDay= (7/0.5);
+        double Days250PerDay= (7/0.25);
+        double Avarage=((Days250PerDay+Days500PerDay)/2);
+        System.out.println("в среднем может понадобиться"+Avarage+"дней, " +
+                "если спортсмен сможет терять в весе от 500 до 250 грамм. ");
+
+        int Masha = 67760;
+        int Denis = 83690;
+        int Kristina = 76230;
+        System.out.println();
+        double MashaIncrease = Masha+ Masha*0.1;
+        double DenisIncrease= Denis+(Denis*0.1);
+        double KristinaIncrease= Kristina+ Kristina*0.1;
+        System.out.println();
+        double DifferanceMasha= ((MashaIncrease*12)-(Masha*12));
+        double DifferanceDenis= ((DenisIncrease*12)-(Denis*12));
+        double DifferanceKristina= ((KristinaIncrease*12)-(Kristina*12));
+        System.out.println("Маша теперь получает "+MashaIncrease+" рублей. Годовой доход вырос на "+DifferanceMasha
+                +" рублей.");
+        System.out.println("Денис теперь получает "+DenisIncrease+" рублей. Годовой доход вырос на "+DifferanceDenis
+                +" рублей.");
+        System.out.println("Денис теперь получает "+KristinaIncrease+" рублей. Годовой доход вырос на "+DifferanceKristina
+                +" рублей. ");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
